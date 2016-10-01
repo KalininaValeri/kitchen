10efa55e69e3516f63f0fbf29935059fa9ccbf40
KalininaValeri <kalinina.valeri@gmail.com>
2016-10-01 20:37:35 +0700

    nav

---
+++ ejs-data.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+    return {
+        menu: [
+            {
+                title: '111',
+                url: 'https://ya.ru'
+            },
+            {
+                title: '222',
+                url: 'https://ya.ru2'
+            },
+            {
+                title: '3313223423423441343',
+                url: 'https://ya.ru3'
+            }
+        ]
+    };
+};
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var ejs = require("gulp-ejs");
 var gutil = require('gulp-util');
 var less = require('gulp-less');
 
+var ejsData = require('./ejs-data');
+
 gulp.task('default', ['watch']);
 
 gulp.task('less', function () {
@@ -12,14 +14,16 @@ gulp.task('less', function () {
 });
 
 gulp.task('ejs', function(){
-    return gulp.src('src/ejs/*.ejs')
-        .pipe(ejs({}, {ext:'.html'}))
+    return gulp.src(['src/ejs/*.ejs', './ejs-data.js'])
+        .pipe(ejs({data: ejsData()}, {ext:'.html'}))
         .pipe(gulp.dest('build/html/'));
 });
 
 gulp.task('watch', function () {
     // gulp.watch('assets/js/libs/**/*.js', ['squish-jquery']);
     // gulp.watch('assets/js/*.js', ['build-js']);
+    gulp.start('less', 'ejs');
+
     gulp.watch('./src/less/**/*.less', ['less']);
     gulp.watch('./src/**/*.ejs', ['ejs']);
 });
